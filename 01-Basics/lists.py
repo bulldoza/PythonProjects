d4d828f27ea3345b257c68c21bade975653cc62a
bulldoza <106199070+bulldoza@users.noreply.github.com>
2026-01-06 12:22:17 -0500

    second edit

--- 01-Basics/lists.py
+++ 01-Basics/lists.py
@@ -1,20 +1,5 @@
 # Lists- one variable that holds multiple values together
 
-cities = ["Phoenix", "Mooresville", "San Diego"]
-city = cities[1]
-print(f"Iam planning to move to {city} next year.")
-
-
-
-services = ["SEO", "Automation", "AI Agents"]
-service = services[1]
-print(f"My business focuses on {service} services.")
-
-stack = ["Python", "FastAPI", "PostgreSQL"]
-tech = stack[1]
-print(f"I am learning {tech} for app development.")
-
-
-schedule = ["morning", "afternoon", "evening"]
-time = schedule[2]
-print(f"I usually study python in the {time}.")
+projects = ["website", "automation tool", "AI app"]
+projects[1]
+print(projects[1])  # Output: automation tool
